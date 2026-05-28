d4860e46d1511694537a26af1bd88581d89fb605
sharath <sharath9042@gmail.com>
2026-05-28 11:05:20 +0530

    update req

--- AI_Resume_Analyzer/AI_Resume_Analyzer/settings.py
+++ AI_Resume_Analyzer/AI_Resume_Analyzer/settings.py
@@ -109,9 +109,9 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default' : dj_database_url.parse(os.getenv('DATABASE_URL')) 
-# }
+DATABASES = {
+    'default' : dj_database_url.parse(os.getenv('DATABASE_URL')) 
+}
 
 
 # Password validation
